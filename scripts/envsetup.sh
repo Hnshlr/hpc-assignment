459f8c923c2a6b9923a3f6823c1a43a2e1017c4f
Hns Hlr <74055973+Hnshlr@users.noreply.github.com>
2023-01-10 10:49:33 +0000

    Update envsetup.sh

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -1,3 +1,2 @@
 # Download public git repo:
-ssh s388885@crescent.central.cranfield.ac.uk "cd /scratch/s388885/; git clone git://github.com/robertoaloi/Android-App-Development.git"
-scp -r ../exports s388885@crescent.central.cranfield.ac.uk:/scratch/s388885/
+ssh s388885@crescent.central.cranfield.ac.uk "cd /scratch/s388885/; ssh -T git@github.com; git clone git@github.com:Hnshlr/hpc-assignment.git"
